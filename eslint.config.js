0715463f837238b8ed4eff8c1b111346232adcb0
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-09-14 14:19:36 +0300

    Add globals package

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,19 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
 
   {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: "readonly",
+      },
+    },
     ignores: ["build", "coverage", "node_modules", "dist", "public", ".git/"],
     rules: {
       "no-unused-vars": "warn",
